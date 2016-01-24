a99669f29886bb8442bb00283864d6680847aa2a
Said <saidozcn@gmail.com>
2016-01-24 11:46:26 +0200

    adds test cases

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,16 +1,19 @@
 import io
 import unittest
 
-import preprocessor
+import preprocessor as p
 
+class PreprocessorTest(unittest.TestCase):
 
-class HelloTest(unittest.TestCase):
+    def test_clean(self):
+        tweet = 'Hello there! @pyistanbul #packathon was awesome. http://packathon.org'
+        cleaned_tweeet = p.clean(tweet)
+        self.assertEqual(cleaned_tweeet, 'Hello there! was awesome.')
 
-    def test_hello(self):
-        output = io.StringIO()
-        preprocessor.say_hello('cihann', file=output)
-        self.assertEqual(output.getvalue(), 'Hello, cihann!\n')
-
+    def test_tokenize(self):
+        tweet = 'Packathon was a really #nice challenging. @packathonorg http://packathon.org'
+        tokenized_tweet = p.tokenize(tweet)
+        self.assertEqual(tokenized_tweet, 'Packathon was a really $HASHTAG$ challenging. $MENTION$ $URL$')
 
 if __name__ == '__main__':
     unittest.main()
