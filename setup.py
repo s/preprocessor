39ef4180815d68e21b15cbbfa7ba7a15ea4e9ebf
Said <saidozcn@gmail.com>
2016-01-24 11:44:25 +0200

    increments version

--- setup.py
+++ setup.py
@@ -5,19 +5,15 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     long_description = f.read()
 
-with open('requirements/requirements.txt') as f:
-    install_requires = [line.strip() for line in f.readlines()]
-
 setup(
-    name='preprocessor',
-    version='0.1',
+    name='tweet-preprocessor',
+    version='0.1.2',
     description='Elegant tweet preprocessing',
     long_description=long_description,
     author='Said Özcan',
     author_email='saidozcn@gmail.com',
     url='https://github.com/s/preprocessor',
     packages=find_packages(),
-    install_requires=install_requires,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
@@ -32,5 +28,5 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    keywords='ast, codegen, PEP8',
+    keywords='machine learning, preprocessing, tweet',
 )
