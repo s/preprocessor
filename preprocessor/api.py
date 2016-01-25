5e128aa8336dc603a6288c9b1411ee09a1b05701
Said <saidozcn@gmail.com>
2016-01-26 01:26:18 +0200

    adds documentation

--- preprocessor/api.py
+++ preprocessor/api.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+preprocessor.api
+~~~~~~~~~~~~
+This module implements the Preprocessor API.
+
+:copyright: (c) 2016 by Said Özcan.
+:license: GPLv3, see LICENSE for more details.
+
+"""
+
 import sys
 from .preprocess import Preprocess
 from .constants import Functions
@@ -7,13 +18,44 @@ preprocessor = Preprocess()
 parser = Parse()
 
 def clean(tweet_string):
+    """Cleans irrelevant information from a tweet text`.
+    :param tweet_string: A tweet text to clean.
+    :return: Cleaned tweet text.
+    :rtype: string
+    Usage::
+      >>> import preprocessor
+      >>> cleaned_tweet = preprocessor.clean("Preprocessor is #awesome https://github.com/s/preprocessor")
+        Preprocessor is
+    """
     cleaned_tweet_string = preprocessor.clean(tweet_string, Functions.CLEAN)
     return cleaned_tweet_string
 
 def tokenize(tweet_string):
+    """Tokenizes irrelevant information in a tweet text`.
+    :param tweet_string: A tweet text to tokenize.
+    :return: Tokenized tweet text.
+    :rtype: string
+    Usage::
+      >>> import preprocessor
+      >>> tokenized_tweet = preprocessor.tokenize("Preprocessor is #awesome https://github.com/s/preprocessor")
+        Preprocessor is $HASHTAG$ $URL$
+    """
     tokenized_tweet_string = preprocessor.clean(tweet_string, Functions.TOKENIZE)
     return tokenized_tweet_string
 
 def parse(tweet_string):
+    """Parses given a tweet text and returns an object`.
+    :param tweet_string: A tweet text to parse.
+    :return: Parsed tweet.
+    :rtype: preprocessor.parse.ParseResult
+    Usage::
+      >>> import preprocessor
+      >>> parsed_tweet = preprocessor.parse("Preprocessor is #awesome https://github.com/s/preprocessor")
+        preprocessor.parse.ParseResult
+      >>> parsed_tweet.urls
+        [(25:58) => https://github.com/s/preprocessor]
+      >>> parsed_tweet.urls[0].start_index
+        25
+    """
     parsed_tweet_obj = parser.parse(tweet_string)
     return parsed_tweet_obj
--- preprocessor/constants.py
+++ preprocessor/constants.py
@@ -1,3 +1,9 @@
+"""
+preprocessor.constants
+~~~~~~~~~~~~
+This module includes the constant variables used in Preprocessor
+"""
+
 class Patterns:
     URL_PATTERN=ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))'
     HASHTAG_PATTERN = r'#\w*'
