3c675c59364ca159fa78cff296c85eb21597a0be
Said <saidozcn@gmail.com>
2016-01-26 02:02:16 +0200

    renames test file and adds parse test

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,5 +15,20 @@ class PreprocessorTest(unittest.TestCase):
         tokenized_tweet = p.tokenize(tweet)
         self.assertEqual(tokenized_tweet, 'Packathon was a really $HASHTAG$ challenging. $MENTION$ $URL$')
 
+    def test_parse(self):
+        tweet = 'A tweet with #hashtag @mention and http://github.com/s.'
+        parsed_tweet = p.parse(tweet)
+
+        self.assertIsNotNone(parsed_tweet.urls)
+        self.assertEqual(1, len(parsed_tweet.urls))
+
+        self.assertIsNotNone(parsed_tweet.hashtags)
+        self.assertEqual(1, len(parsed_tweet.hashtags))
+
+        self.assertIsNotNone(parsed_tweet.mentions)
+        self.assertEqual(1, len(parsed_tweet.mentions))
+
+        self.assertIsNone(parsed_tweet.reserved_words)
+        
 if __name__ == '__main__':
     unittest.main()
