131ce077efb4d4b6f1f0e2587d0fcfd1b14b5d29
Said <saidozcn@gmail.com>
2016-02-02 18:38:57 +0200

    updates smiley regex for additional smileys

--- preprocessor/defines.py
+++ preprocessor/defines.py
@@ -42,5 +42,5 @@ class Patterns:
         # UCS-2
         EMOJIS_PATTERN = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
 
-    SMILEYS_PATTERN = re.compile(r"(?::|;|=)(?:-)?(?:\)|\(|D|P|S){1,}")
+    SMILEYS_PATTERN = re.compile(r"(?:X|:|;|=)(?:-)?(?:\)|\(|O|D|P|S){1,}", re.IGNORECASE)
     NUMBERS_PATTERN = re.compile(r"(^|\s)(\-?\d+(?:\.\d)*|\d+)")
